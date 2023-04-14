72eba2d8b453cc0bad35039a56a2d7db2b6e4873
muneunji <ansdmswl0722@gmail.com>
2023-04-14 17:25:53 +0900

    todo finish

--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvScores.java
@@ -1,27 +1,53 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CsvScores implements Scores {
+    private String scoreCsvUrl = "/Users/muneunji/Downloads/springframework-project1/src/main/resources/data/score.csv";
+    private List<Score> scoreList;
 
     private CsvScores(){}
 
     /** TODO 2 :
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
      **/
+    private static class SingleInstancesHolder {
+        private static final CsvScores INSTANCE = new CsvScores();
+    }
     public static Scores getInstance() {
-        return null;
+        return SingleInstancesHolder.INSTANCE;
     }
 
     // TODO 5 : score.csv 파일에서 데이터를 읽어 멤버 변수에 추가하는 로직을 구현하세요.
     @Override
     public void load() {
-
+        try(InputStream inputStream = new FileSystemResource(scoreCsvUrl).getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        ) {
+            String line = "";
+            scoreList = new ArrayList<>();
+            while ((line=reader.readLine()) != null) {
+                String[] array = line.split(",");
+                int seq = Integer.parseInt(array[0]);
+                int score = Integer.parseInt(array[1]);
+                Score scoreObject = new Score(seq,score);
+                scoreList.add(scoreObject);
+            }
+            System.out.println(scoreList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public List<Score> findAll() {
-        return null;
+        return scoreList;
     }
 }
--- src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
+++ src/main/java/com/nhnacademy/edu/springframework/project/repository/CsvStudents.java
@@ -1,32 +1,57 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
 import com.nhnacademy.edu.springframework.project.service.Student;
+import org.springframework.core.io.FileSystemResource;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
-
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class CsvStudents implements Students {
 
+    private String studentCsvUrl = "/Users/muneunji/Downloads/springframework-project1/src/main/resources/data/student.csv";
+    private Map<Integer,Student> studentMap;
+    private CsvStudents(){}
 
     /** TODO 3 :
      * Java Singleton 패턴으로 getInstance() 를 구현하세요.
      **/
+    private static class SingleInstancesHolder {
+        private static final CsvStudents INSTANCE = new CsvStudents();
+    }
     public static Students getInstance() {
-        return null;
+        return SingleInstancesHolder.INSTANCE;
     }
 
     // TODO 7 : student.csv 파일에서 데이터를 읽어 클래스 멤버 변수에 추가하는 로직을 구현하세요.
     // 데이터를 적재하고 읽기 위해서, 적절한 자료구조를 사용하세요.
     @Override
     public void load() {
-
+        try(InputStream inputStream = new FileSystemResource(studentCsvUrl).getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        ){
+            String line ="";
+            studentMap = new HashMap<>();
+            while ((line=reader.readLine()) != null) {
+                String[] array = line.split(",");
+                int seq = Integer.parseInt(array[0]);
+                Student student = new Student(seq,array[1]);
+                studentMap.put(seq,student);
+            }
+            System.out.println(studentMap);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Collection<Student> findAll() {
-        return null;
+        return studentMap.values();
     }
 
     /**
@@ -35,6 +60,10 @@ public class CsvStudents implements Students {
      */
     @Override
     public void merge(Collection<Score> scores) {
-
+       scores.stream()
+               .forEach(score -> {
+                   Student student = studentMap.get(score.getStudentSeq());
+                   student.setScore(score);
+               });
     }
 }
